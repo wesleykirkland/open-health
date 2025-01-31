b1c2d6c0938f8e16215dfbf27cecbfd83c848362
GwonHyeok <me@ghyeok.io>
2025-01-31 12:42:32 +0900

    llmProvider seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,18 @@
 import {PrismaClient} from '@prisma/client'
 import assistantModeSeed from './data/assistant-mode.json'
+import llmProviderSeed from './data/llm-provider.json'
+
 
 const prisma = new PrismaClient()
 
 async function main() {
     await prisma.assistantMode.createMany({
         data: assistantModeSeed,
+        skipDuplicates: true
+    });
+
+    await prisma.lLMProvider.createMany({
+        data: llmProviderSeed,
         skipDuplicates: true
     });
 
