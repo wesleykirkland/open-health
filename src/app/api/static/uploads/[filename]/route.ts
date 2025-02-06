3d5f98ab015dd7c2e60f44ff186a0999488bf231
GwonHyeok <me@ghyeok.io>
2025-02-07 01:13:11 +0900

    support file upload

---
+++ src/app/api/static/uploads/[filename]/route.ts
@@ -0,0 +1,24 @@
+import {NextRequest} from "next/server";
+import fs from "fs/promises";
+
+export async function GET(
+    req: NextRequest,
+    {params}: { params: Promise<{ filename: string }> }
+) {
+    const {filename} = await params
+
+    if (!filename.match(/^[a-zA-Z0-9-_]+\.pdf$/)) {
+        return new Response('Invalid filename', {status: 400})
+    }
+
+    const filePath = `./public/uploads/${filename}`
+    const file = await fs.readFile(filePath)
+
+    // Response blob
+    return new Response(file, {
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Content-Disposition': `attachment; filename=${filename}`
+        }
+    })
+}
