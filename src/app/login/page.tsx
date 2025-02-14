a26bf9cfe310daa8d082a236e113fdd29d56bb77
GwonHyeok <me@ghyeok.io>
2025-02-15 03:32:37 +0900

    fix build error login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,10 @@
 import LoginScreen from '@/components/auth/login-screen';
+import {Suspense} from "react";
 
 export default function LoginPage() {
-  return <LoginScreen />;
-} 
+    return (
+        <Suspense>
+            <LoginScreen/>
+        </Suspense>
+    )
+}
