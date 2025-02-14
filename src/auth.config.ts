614c7b53ed98e03cc5027d0b29ce35d9cc5df91f
GwonHyeok <me@ghyeok.io>
2025-02-14 18:58:11 +0900

    implement auth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,18 @@
 import {NextAuthConfig} from "next-auth";
 
 export const authConfig = {
-    callbacks: {},
-    providers: []
+    session: {
+        strategy: 'jwt',
+    },
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        session: async ({session, token}) => {
+            if (token.sub && session.user) session.user.id = token.sub;
+            return session;
+        },
+    },
+    providers: [],
 } satisfies NextAuthConfig;
 
