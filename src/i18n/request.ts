2f708cf3d02983d348621567a0def3460f9bec3b
nevermind <nevermind@kaist.ac.kr>
2025-02-19 22:49:03 +0900

    add translation

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,7 +3,7 @@ import {headers} from "next/headers";
 import Negotiator from 'negotiator'
 
 export default getRequestConfig(async () => {
-    const availableLanguages = ['ko', 'en'];
+    const availableLanguages = ['ko', 'en', 'ru', 'uk', 'de', 'fr', 'es', 'ja', 'zh', 'ur'];
     const acceptLanguage = (await headers()).get('accept-language');
     const negotiator = new Negotiator({headers: {'accept-language': acceptLanguage || ''}});
     const locale = negotiator.language(availableLanguages) || 'en';
